80a05e21762cc2886d780ccca3aac869e509020d
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-15 01:36:33 +0200

    error fixed & data added in constants.js

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Card = () => {
+const Card = ({ ques }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   function handleClick() {
@@ -8,9 +8,19 @@ const Card = () => {
   }
 
   return isVisible ? (
-    <div onClick={handleClick} className="m-1 w-72 h-20 bg-red-200"></div>
+    <div
+      onClick={handleClick}
+      className="m-1 p-2 w-72 h-20 text-lg font-bold font-mono leading-5 bg-red-200 flex justify-center items-center"
+    >
+      <p>{ques.answer}</p>
+    </div>
   ) : (
-    <div onClick={handleClick} className="m-1 w-72 h-20 bg-green-200"></div>
+    <div
+      onClick={handleClick}
+      className="m-1 p-2 w-72 h-20 font-mono leading-5 bg-green-200 flex justify-center items-center"
+    >
+      <p>{ques.question}</p>
+    </div>
   );
 };
 
--- src/FlashCards.js
+++ src/FlashCards.js
@@ -1,17 +1,16 @@
 import Card from "./Card";
+import { questions } from "./utils/constants";
 
 const FlashCards = () => {
   return (
     <div>
-      <h1 className="mt-5 text-center font-mono text-5xl font-extrabold">
+      <h1 className="mt-5 text-center font-mono text-4xl font-extrabold">
         Flash-Cards
       </h1>
       <div className="px-5 py-5 w-full h-full flex flex-wrap justify-center items-center">
-        {Array(5)
-          .fill()
-          .map((i) => (
-            <Card />
-          ))}
+        {questions.map((q) => (
+          <Card ques={q} key={q.id} />
+        ))}
       </div>
     </div>
   );
